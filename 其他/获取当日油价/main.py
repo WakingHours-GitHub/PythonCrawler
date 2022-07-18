56d78e9c41c1faa4c361a541d6f9266a8a70c470
WakingHours-GitHub <WakingHoursHUC@outlook.com>
2022-07-18 19:23:19 +0800

    7. 18

--- 其他/获取当日油价/main.py
+++ 其他/获取当日油价/main.py
@@ -6,11 +6,12 @@ import os
 def main() -> None:
     is_change = False
     lines = None
-    oil_price_list2list = get_oil_price()
-    print(oil_price_list2list[1:]) # [['黑龙江', '9.03', '9.67', '10.97', '2022/7/5']]
+    oil_price_list2list = get_oil_price() # 获取最新油价.
+    print(oil_price_list2list[1:]) # [['黑龙江', '8.74', '9.36', '10.62', '2022/7/15']]
 
     # 没有就写入
     if not os.path.exists("./data.txt"): # 如果没有文件, 则直接写入创建一个
+        print("no directory, creat 'data.txt'")
         with open("./data.txt", "wt", encoding='utf-8') as f_write:
             for tr_list in oil_price_list2list:
                 f_write.write(",".join(tr_list))
@@ -29,22 +30,24 @@ def main() -> None:
         #         break
 
         # print(is_change)
-
+        # 对比.
         if is_change:  # 只有油价改变时, 才发送邮件.
             print(lines) # [['黑龙江', '9.03', '9.67', '10.97', '2022/7/4']]
 
             for i in range(len(oil_price_list2list[1: ])): # 注意数组越界。
                 for new_price, old_price in tuple(zip(oil_price_list2list[i+1], lines[i]))[1 :-1]:
-
+                    # if new_price  old_price:
+                    #     pass
                     print(new_price)
                     print(old_price)
 
-            return
+            # return # 中断, 测试用
 
 
+            # 发送邮件
             print("oil price change, send email")
             send_email(conversation(oil_price_list2list), "html")
-            # 保存到文件当中
+            # 更新到文件当中
             with open("data.txt", "wt", encoding='utf-8') as f_write:
                 for tr_list in oil_price_list2list:
                     f_write.write(",".join(tr_list))
